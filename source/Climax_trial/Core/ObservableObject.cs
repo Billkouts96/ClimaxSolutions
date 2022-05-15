ac554f56cb1ca639afaf4fa50c4488b749498536
Christoforos Vasilis Koutsianoudis <48958980+Billkouts96@users.noreply.github.com>
2022-05-15 15:40:14 +0300

    Commit after jointly coding
    Added functionality and new window

--- source/Climax_trial/Core/ObservableObject.cs
+++ source/Climax_trial/Core/ObservableObject.cs
@@ -12,6 +12,19 @@ namespace Climax_trial.Core
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
-        
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+
     }
 }
---
+++ source/Climax_trial/MVVM/ViewModel/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Climax_trial.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Climax_trial.MVVM.ViewModel
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            DataContext = new DataAccess();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
