42b602f5998b45b533e49d2e80c99192a0df4b0b
Christoforos Vasilis Koutsianoudis <48958980+Billkouts96@users.noreply.github.com>
2022-05-15 15:39:02 +0300

    Create DataAccess.cs

---
+++ source/Climax_trial/MVVM/ViewModel/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Climax_trial.MVVM.Model
+{
+
+    public class DataAccess
+    {
+        public List<string> COMPANY_TYPES { get; set; }
+        public DataAccess() {
+             COMPANY_TYPES = new List<string>()
+            { "Ατομική επιχείρηση",
+                "Ομόρρυθμη εταιρία",
+                "Ετερόρρυθμη εταιρία",
+                "ΑΕ",
+               "Μονοπρόσωπη ΙΚΕ",
+                "ΙΚΕ",
+                "ΚΟΙΝΣΕΠ" };
+    }
+    }
+}
+
+    
+          
+
+
